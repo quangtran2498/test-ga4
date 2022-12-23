f4cba7aedb5b292bd8e0f24bcdad7bc33730efe5
Quang Trần <tranmanhquang2498it@gmail.com>
2022-12-23 16:52:27 +0700

    dasdas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-QLLWFJT0CW");
+const OnGa = () => {
+  ReactGA.event({
+    category: "your category",
+    action: "your action",
+    label: "your label", // optional
+    value: 99, // optional, must be a number
+    nonInteraction: true, // optional, true/false
+    transport: "xhr", // optional
+    quangtest: "1234", //! ko nhận
+    dimension: "quang test",
+  });
+  ReactGA.gtag("event", "quang test", {
+    event_action: "quang test gtag",
+  });
+};
 function App() {
   return (
     <div className="App">
-      <div>quang test ga4 </div>
+      <div onClick={OnGa}>quang test ga4 </div>
     </div>
   );
 }
