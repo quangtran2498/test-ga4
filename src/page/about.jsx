2eaae7ba31822938ad951189102daa9d26d0f6e3
Quang Trần <tranmanhquang2498it@gmail.com>
2022-12-29 10:36:49 +0700

    dadas

--- src/page/about.jsx
+++ src/page/about.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import ReactGA from "react-ga4";
-// ReactGA.initialize("G-VZ6T7SQ29E");
+ReactGA.initialize("G-VZ6T7SQ29E");
+ReactGA.send("pageview");
+
 const About = () => {
   const navigate = useNavigate();
 
