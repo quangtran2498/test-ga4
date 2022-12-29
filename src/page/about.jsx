1ee95a601247e013ca9c2435aacda45e8ba53f3d
Quang Trần <tranmanhquang2498it@gmail.com>
2022-12-29 10:07:18 +0700

    dasdsa

--- src/page/about.jsx
+++ src/page/about.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import ReactGA from "react-ga4";
-
+ReactGA.initialize("G-VZ6T7SQ29E");
 const About = () => {
   const navigate = useNavigate();
 
