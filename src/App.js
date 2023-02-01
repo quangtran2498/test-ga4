1124a893f07c7c1f5876b07622fc093a6a8daeb8
Quang Trần <tranmanhquang2498it@gmail.com>
2023-02-01 15:22:06 +0700

    dasdsa

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import About from "./page/about";
 import Dasboard from "./page/dasboard";
 import gtag from "./page/gtag";
 //!
-ReactGA.initialize("G-VZ6T7SQ29E");
+// ReactGA.initialize("G-VZ6T7SQ29E");
+ReactGA.initialize("G-GL0NXS0STE");
+
 // ReactGA.send("pageview");
 const createEvent = () => {
   // ReactGA.event({
@@ -23,21 +25,32 @@ const createEvent = () => {
     event_action: "quang test gtag",
     abc: "31312312312",
   });
+  console.log("get time", new Date().getTime());
 };
 const createEvent2 = () => {
   ReactGA.gtag("event", "create event", {
     event_action: "quang test gtag",
     abc: "hihi",
   });
+  console.log("get time", new Date().getTime());
 };
 const createEvent3 = () => {
   gtag("event", "create event gtag", {
     test: "12132131231",
   });
 };
+var test = { abc: new Date().getTime() };
+var getTime = new Date().getTime();
+var items = [1, 2, 3, 4, 5, 6, 7];
+const ramdomNumber = () => {
+  var item = items[Math.floor(Math.random() * items.length)];
+  console.log(item);
+};
+// const ramdomNumber = () => {
+//   var abc = items.random();
+//   console.log("abc", abc);
+// };
 
-// console.log("dasda", ReactGA.gtag);
-// console.log("dasdsa", window["gtag"]);
 function App() {
   return (
     <div className="App">
@@ -50,7 +63,14 @@ function App() {
       <div style={style} onClick={createEvent3}>
         create event 3
       </div>
-
+      <button onClick={ramdomNumber}>click ramdom number</button>
+      <div class="test"></div>
+      <div id="circle">
+        <div class="segment"></div>
+        <div class="segment"></div>
+        <div class="segment"></div>
+        <div class="segment"></div>
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
