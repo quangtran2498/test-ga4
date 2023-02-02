96788b25676c2e9b88b67afffa198b0bd04c7bcd
Quang Trần <tranmanhquang2498it@gmail.com>
2023-02-02 11:20:07 +0700

    DADAS

--- src/App.js
+++ src/App.js
@@ -7,20 +7,36 @@ import Dasboard from "./page/dasboard";
 import gtag from "./page/gtag";
 //!
 ReactGA.initialize("G-VZ6T7SQ29E");
+const measurement_id = "G-VZ6T7SQ29E"
+const api_secret = "dH0ZWqTvReWoYTUJiVK9Vw"
 // ReactGA.initialize("G-GL0NXS0STE");
-
+ const testApiGa = () => {
+  fetch(`https://www.google-analytics.com/mp/collect?measurement_id=${measurement_id}&api_secret=${api_secret}`, {
+  method: "POST",
+  body: JSON.stringify({
+    client_id: '12345.12345',
+    events: [{
+      name: 'push event',
+      params: {
+        quangTestPushEvent:"da lam"
+      },
+    }]
+  })
+});
+ }
 // ReactGA.send("pageview");
 const createEvent = () => {
-  ReactGA.event({
-    category: "your category",
-    action: "your action",
-    label: "your label", // optional
-    value: 99, // optional, must be a number
-    nonInteraction: true, // optional, true/false
-    transport: "xhr", // optional
-    // quangtest: "1234", //! ko nhận
-    dimension: "quang test",
-  });
+  // ReactGA.event({
+  //   category: "your category",
+  //   action: "your action",
+  //   label: "your label", // optional
+  //   value: 99, // optional, must be a number
+  //   nonInteraction: true, // optional, true/false
+  //   transport: "xhr", // optional
+  //   // quangtest: "1234", //! ko nhận
+  //   dimension: "quang test",
+  // });
+  testApiGa()
   ReactGA.gtag("event", "quang test", {
     event_action: "quang test gtag",
     abc: "31312312312",
@@ -29,6 +45,7 @@ const createEvent = () => {
   });
 };
 const createEvent2 = () => {
+  testApiGa()
   ReactGA.gtag("event", "create event", {
     event_action: "quang test gtag",
     abc: "hihi",
@@ -38,6 +55,7 @@ const createEvent2 = () => {
   // console.log("get time", new Date().getTime());
 };
 const createEvent3 = () => {
+  testApiGa()
   gtag("event", "create event gtag", {
     test: "12132131231",
     engagement_time_msec: new Date().getTime(),
