50ca1df3358e3f7f9e75220d09ff9d1da7ddeb78
Quang Trần <tranmanhquang2498it@gmail.com>
2023-02-02 10:38:22 +0700

    dasdas

--- src/App.js
+++ src/App.js
@@ -11,51 +11,40 @@ ReactGA.initialize("G-VZ6T7SQ29E");
 
 // ReactGA.send("pageview");
 const createEvent = () => {
-  // ReactGA.event({
-  //   category: "your category",
-  //   action: "your action",
-  //   label: "your label", // optional
-  //   value: 99, // optional, must be a number
-  //   nonInteraction: true, // optional, true/false
-  //   transport: "xhr", // optional
-  //   quangtest: "1234", //! ko nhận
-  //   dimension: "quang test",
-  // });
+  ReactGA.event({
+    category: "your category",
+    action: "your action",
+    label: "your label", // optional
+    value: 99, // optional, must be a number
+    nonInteraction: true, // optional, true/false
+    transport: "xhr", // optional
+    // quangtest: "1234", //! ko nhận
+    dimension: "quang test",
+  });
   ReactGA.gtag("event", "quang test", {
     event_action: "quang test gtag",
     abc: "31312312312",
-    engagement_time_msec: "100",
+    engagement_time_msec: new Date().getTime(),
     session_id: "123",
   });
-  console.log("get time", new Date().getTime());
 };
 const createEvent2 = () => {
   ReactGA.gtag("event", "create event", {
     event_action: "quang test gtag",
     abc: "hihi",
-    engagement_time_msec: "100",
+    engagement_time_msec: new Date().getTime(),
     session_id: "123",
   });
-  console.log("get time", new Date().getTime());
+  // console.log("get time", new Date().getTime());
 };
 const createEvent3 = () => {
   gtag("event", "create event gtag", {
     test: "12132131231",
-    engagement_time_msec: "100",
+    engagement_time_msec: new Date().getTime(),
     session_id: "123",
   });
 };
-var test = { abc: new Date().getTime() };
-var getTime = new Date().getTime();
-var items = [1, 2, 3, 4, 5, 6, 7];
-const ramdomNumber = () => {
-  var item = items[Math.floor(Math.random() * items.length)];
-  console.log(item);
-};
-// const ramdomNumber = () => {
-//   var abc = items.random();
-//   console.log("abc", abc);
-// };
+
 
 function App() {
   return (
@@ -68,14 +57,6 @@ function App() {
       </div>
       <div style={style} onClick={createEvent3}>
         create event 3
-      </div>
-      <button onClick={ramdomNumber}>click ramdom number</button>
-      <div class="test"></div>
-      <div id="circle">
-        <div class="segment"></div>
-        <div class="segment"></div>
-        <div class="segment"></div>
-        <div class="segment"></div>
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
